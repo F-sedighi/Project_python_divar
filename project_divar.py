ab35640d54e86583326dbaa2f8c5875d854b1509
F-sedighi <122398831+F-sedighi@users.noreply.github.com>
2025-02-11 08:53:56 +0330

    q::qkukukukukukdkdkdkdkdkdkdkdkukukukukukukukdkdkdkdkdkdkukukukdkdkdkdkdkdkukukdkdkdkdkdkdkukukukukdkdkdkdkukukukukdkdkdkdkukukukukukdkdkdkdkdkukukukukukukukukukukukdkdkdkdkdkdkdkdklkrýaikbkbkdkdkdkdkdkdkdkdkdkdkdkdkdkukdýa:q~!ýaiýa:q!

--- project_divar.py
+++ project_divar.py
@@ -1,4 +1,11 @@
 import requests
 from bs4 import BeautifulSoup
 
-response = requests.get('https://divar.ir/s/kurdistan-province')
+response = requests.get('https://divar.ir/s/kurdistan-province/car/peugeot/405')
+soup = BeautifulSoup(response.text, 'html.parser')
+res1 = soup.find_all('h2', attrs = {'class': 'kt-post-card__title'})
+res2 = soup.find_all(attrs={'class':{'kt-post-card__description', 'kt-post-card__title', 'kt-post-card__bottom-description kt-text-truncate'}})
+[print(str2.get_text()) for str2 in res2]
+
+
+#[print(str1.get_text(), str2.get_text()) for str1,str2 in zip(res1,res2)]
