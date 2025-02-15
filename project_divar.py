d3b1fc400eecbd053cc1fab54fd4ade88b091d9a
F-sedighi <sedighifardin@gmail.com>
2025-02-16 03:14:10 +0330

    ýaýaýai:kbýa:q!

--- project_divar.py
+++ project_divar.py
@@ -4,8 +4,7 @@ from bs4 import BeautifulSoup
 response = requests.get('https://divar.ir/s/kurdistan-province/car/peugeot/405')
 soup = BeautifulSoup(response.text, 'html.parser')
 res1 = soup.find_all('h2', attrs = {'class': 'kt-post-card__title'})
+#res2 = soup.find_all('div', attrs = {'class': 'kt-post-card__info'})
 res2 = soup.find_all(attrs={'class':{'kt-post-card__description', 'kt-post-card__title', 'kt-post-card__bottom-description kt-text-truncate'}})
-[print(str2.get_text()) for str2 in res2]
 
-
-#[print(str1.get_text(), str2.get_text()) for str1,str2 in zip(res1,res2)]
+x=[string for iter in range(len(res2)) for string in res2[iter].stripped_strings]
